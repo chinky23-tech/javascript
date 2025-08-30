99783e7bfb7f89c29d7e42b3acba30a10991cf78
chinky23-tech <chinky0330@gmail.com>
2025-08-31 00:43:12 +0530

    added color

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-const myImage = document.querySelector("img");
+const button = document.getElementById('colorBtn');
+const h1 = document.querySelector('h1');
+function getRandomColor(){
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i< 6; i++){
+        color += letters[Math.floor(Math.random()*16)];
+        
+    }
+    return color;
+}
+button.addEventListener('click', function(){
+document.body.style.backgroundColor = getRandomColor();
+});
+button.addEventListener('click',() => {
+const color1 = getRandomColor();
+const color2 = getRandomColor();
+const c = getRandomColor();
+document.body.style.backgroundColor = c;
+h1.textContent = `Current color: ${c}`;
 
-myImage.addEventListener("click", () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/pasta-variation.jpg") {
-    myImage.setAttribute("src", "images/coffeehouse.png");
-  } else {
-    myImage.setAttribute("src", "images/pasta-variation.jpg");
-  }
+document.body.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
 });
